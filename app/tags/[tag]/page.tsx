22f6d0c4839abdc5ff27e9c36daeccd6ff9082bf
kei-s16 <kei1216s@nifty.com>
2023-07-19 12:59:08 +0900

    ペジネーション追加

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -1,5 +1,6 @@
 import type { NextPage, Metadata} from "next";
 import Link from "next/link";
+import { redirect } from 'next/navigation'
 import Card from "modules/article/components/card";
 import { getCurrentTagPage } from "modules/pages/functions";
 
@@ -23,6 +24,12 @@ const Home: NextPage = (
   }
 ) => {
   const tag = decodeURIComponent(params.tag);
+
+  // FIXME: こんなケースはなさそうな気がする
+  if(!tag) {
+    redirect("/tags")
+  }
+
   const index = 1 < searchParams.page ? searchParams.page - 1 : TOP_PAGE_INDEX;
   const { posts, prev, next } = getCurrentTagPage(tag, index);
 
@@ -34,9 +41,9 @@ const Home: NextPage = (
         ))}
       </div>
       <div className="flex flex-row content-center justify-center">
-          <div className="basis-1/3 rounded-lg flex items-center justify-center">{prev && <Link href={`/?page=${prev}`}>previous</Link>}</div>
+          <div className="basis-1/3 rounded-lg flex items-center justify-center">{prev && <Link href={`/tags/${encodeURIComponent(tag)}?page=${prev}`}>previous</Link>}</div>
           <div className="basis-1/3 rounded-lg flex items-center justify-center"><Link href="/">◆</Link></div>
-          <div className="basis-1/3 rounded-lg flex items-center justify-center">{next && <Link href={`/?page=${next}`}>next</Link>}</div>
+          <div className="basis-1/3 rounded-lg flex items-center justify-center">{next && <Link href={`/tags/${encodeURIComponent(tag)}?page=${next}`}>next</Link>}</div>
       </div>
     </>
   );
